1ec95d3b3bee7d30a3b825fed78e4120cf1611ba
Jumpy <poudjoumrodrigue@gmail.com>
2024-08-24 23:06:46 +0100

    merci

--- src/app/pages/reset-password/reset-password.component.ts
+++ src/app/pages/reset-password/reset-password.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { ResetPasswordRequest } from '../../interfaces/reset-password-request';
+import { AuthService } from '../../services/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-reset-password',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule,FormsModule,MatSnackBarModule],
   templateUrl: './reset-password.component.html',
   styleUrl: './reset-password.component.css'
 })
-export class ResetPasswordComponent {
+export class ResetPasswordComponent implements OnInit{
+  
+  resetPassword ={} as ResetPasswordRequest;
+  authService =inject(AuthService);
+   router = inject(Router);
+   route = inject(ActivatedRoute);
+   matSnackBar =inject(MatSnackBar);
+
+   ngOnInit(): void {
+    this.route.queryParams.subscribe((params)=>{
+      this.resetPassword.email=params["email"];
+      this.resetPassword.token=params["token"];
+    })
+  }
+
+  resetPasswordHandle(){
+    console.log('Reset password initiated:', this.resetPassword);
+    this.authService.resetPassword(this.resetPassword).subscribe({
+      next: (response)=>{
+        console.log('Password reset successful:', response);
+        this.matSnackBar.open(response.message,'Close',{
+          duration:5000,
+        });
+        this.router.navigate(['/login']);
+      },
+      error :(error:HttpErrorResponse)=>{
+        console.error('Password reset error:', error);
+        this.matSnackBar.open(error.error.message,'Close',{
+          duration:5000,
+        });
+      }
+    })
+
+  }
+
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,6 +7,7 @@ import { HttpClient } from '@angular/common/http';
 import { jwtDecode } from 'jwt-decode';
 import { RegisterRequest } from '../interfaces/register-request';
 import { UserDetail} from '../interfaces/user-detail';
+import { ResetPasswordRequest } from '../interfaces/reset-password-request';
 
 @Injectable({
   providedIn: 'root'
@@ -89,5 +90,8 @@ export class AuthService {
    }
 
    forgotPassword =(email:string):Observable<AuthResponse> =>
-    this.http.post<AuthResponse>(`${this.apiUrl}/Account/forgot-password`,{email,});
+    this.http.post<AuthResponse>(`${this.apiUrl}/Account/forgot-password`,{email});
+
+   resetPassword =(data:ResetPasswordRequest):Observable<AuthResponse> =>
+    this.http.post<AuthResponse>(`${this.apiUrl}/Account/reset-password`,data);
 }
